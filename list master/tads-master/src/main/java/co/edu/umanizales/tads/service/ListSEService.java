8dd58b0491dac3afc2b8ece622e12f561901c79a
JuanERivera <127379867+JuanERivera@users.noreply.github.com>
2023-04-17 16:56:43 -0500

    Add files via upload
    Se corrigieron algunos métodos antiguos que no funcionaban, se crearon unos nuevos, se creo un nuevo DTO, entre otras cosas, aunque no pude acabar con todo lo solicitado por falta de tiempo

---
+++ list master/tads-master/src/main/java/co/edu/umanizales/tads/service/ListSEService.java
@@ -0,0 +1,33 @@
+package co.edu.umanizales.tads.service;
+
+import co.edu.umanizales.tads.model.Kid;
+import co.edu.umanizales.tads.model.ListSE;
+import co.edu.umanizales.tads.model.Node;
+import lombok.Data;
+import org.springframework.stereotype.Service;
+
+@Service
+@Data
+public class ListSEService {
+    private ListSE kids;
+
+    public ListSEService() {
+        kids = new ListSE();
+
+    }
+
+    public void invert() {
+        kids.invert();
+    }
+
+
+    public void addToStart(Kid kid) {
+        kids.addToStart(kid);
+    }
+    public void addByPosition(int position, Kid kid){
+        kids.addByPosition(kid);
+    }
+    public void removeByCode(String identification){
+        kids.removeByCode(identification);
+    }
+}
